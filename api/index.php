dc3a38a8b58c6540d8743b071c1d786fa6ee8170
lmontes18 <lesy_montesz40@unicah.edu>
2026-03-20 17:15:38 -0600

    me canso

--- api/index.php
+++ api/index.php
@@ -4,5 +4,8 @@
 if (!is_dir('/tmp/views')) {
     mkdir('/tmp/views', 0755, true);
 }
+putenv("VIEW_COMPILED_PATH=/tmp/views");
+putenv("SESSION_DRIVER=cookie"); 
+putenv("LOG_CHANNEL=stderr");
 
 require __DIR__ . '/../public/index.php';
