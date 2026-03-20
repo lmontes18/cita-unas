4a551726be44291a6de460f931baa443ce4019ee
lmontes18 <lesy_montesz40@unicah.edu>
2026-03-20 13:31:39 -0600

    mas cambios vo

--- api/index.php
+++ api/index.php
@@ -1,5 +1,8 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+
+// Forzar la creación de la carpeta de vistas en el entorno temporal de Vercel
+if (!is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0755, true);
+}
 
 require __DIR__ . '/../public/index.php';
