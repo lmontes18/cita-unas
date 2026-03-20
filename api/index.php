bb593b8f54aa54d00d0bc5878606f76e7eb19786
lmontes18 <lesy_montesz40@unicah.edu>
2026-03-20 17:20:13 -0600

    unomaaaaas

--- api/index.php
+++ api/index.php
@@ -1,11 +1,26 @@
 <?php
 
-// Forzar la creación de la carpeta de vistas en el entorno temporal de Vercel
-if (!is_dir('/tmp/views')) {
-    mkdir('/tmp/views', 0755, true);
+// Crear carpetas en /tmp
+$storagePaths = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/bootstrap/cache',
+];
+
+foreach ($storagePaths as $path) {
+    if (!is_dir($path)) {
+        mkdir($path, 0755, true);
+    }
 }
-putenv("VIEW_COMPILED_PATH=/tmp/views");
-putenv("SESSION_DRIVER=cookie"); 
-putenv("LOG_CHANNEL=stderr");
+
+// Configuración de emergencia para Laravel en Serverless
+putenv('APP_CONFIG_CACHE=/tmp/storage/bootstrap/cache/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/storage/bootstrap/cache/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/storage/bootstrap/cache/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/storage/bootstrap/cache/packages.php');
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+putenv('CACHE_DRIVER=array');
+putenv('SESSION_DRIVER=cookie');
 
 require __DIR__ . '/../public/index.php';
